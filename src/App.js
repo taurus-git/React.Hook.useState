7d3703fc6d9f860acce7b38035c367cb86ef46c2
taurus-git <t.1113@ya.ru>
2020-08-31 09:40:26 +0300

    Navigation created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Route from "./components/Route";
+import Header from "./components/Header";
 
 const items = [
     {
@@ -35,23 +37,29 @@ const options = [
 ];
 
 export default () => {
-    /*const [selected, setSelected] = useState(options[0]);
-    const [showDropdown, setDropdown] = useState(true);*/
+    const [selected, setSelected] = useState(options[0]);
+    const [showDropdown, setDropdown] = useState(true);
 
     return (
         <div>
-            <Translate />
-            {/*<button onClick={() => setDropdown(!showDropdown)}>Toggle Dropdown</button>
-            {showDropdown ? (
+            <Header />
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
                 <Dropdown
-                selected={selected}
-                onSelectedChange={setSelected}
-                options={options}
+                    label="Select a color"
+                    options={options}
+                    selected={selected}
+                    onSelectedChange={setSelected}
                 />
-                ) : null
-            }*/}
-            {/*<Search />*/}
-            {/*<Accordion items={items}/>*/}
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
         </div>
     );
 };
